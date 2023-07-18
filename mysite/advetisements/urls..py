48d238add54a171b1f16f75be5dc577cb02d8762
lastdarkHero <kkerra9@gmail.com>
2023-07-18 18:27:50 +0300

    урок 4,5

--- mysite/advetisements/urls..py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import index
-
-urlpatterns = [
-    path("hello-world/", index)
-]
--- mysite/advetisements/views.py
+++ mysite/advetisements/views.py
@@ -2,4 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Все топчик")
+    return render(request, "index.html")
+
+
+def top_sellers(request):
+    return render(request, "top-sellers.html")
